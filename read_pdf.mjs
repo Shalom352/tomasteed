c16a80c6e64bd2dfee1ae81343fc266a42139ef7
Shalom FOLY <shalom.foly@tomasteed.com>
2026-05-26 14:12:07 +0000

    Nettoyage : suppression fichiers temporaires du repo

--- read_pdf.mjs
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs';
-import pdf from 'pdf-parse/lib/pdf-parse.js';
-
-const buf = readFileSync('TOMASTEED-doc-a lire.pdf');
-const data = await pdf(buf);
-console.log('Pages:', data.numpages);
-console.log(data.text.substring(0, 12000));
